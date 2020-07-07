337d08b560b155bcbff828a0acb8b23f2e21db4f
bashadmin <dev.sugarman@gmail.com>
2020-07-07 07:43:27 -0400

    Added the original print statement.

--- Lab7.py
+++ Lab7.py
@@ -4,4 +4,4 @@ print(x)
 mixedList = [55, 23432, 349.50, False, True, "Hello world", [1,2,3,'a','b'], "hi", 2, x]
 for item in mixedList:
     print(f"{item} is of data type {type(item)}")
-    
+    print("{} is of the data type {}".format(item, type(item)))
