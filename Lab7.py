efe757b859f6986d7e34d1309977dd5b20520421
bashadmin <dev.sugarman@gmail.com>
2020-07-07 07:01:50 -0400

    Initial Commit

---
+++ Lab7.py
@@ -0,0 +1,7 @@
+from random import randint as ri
+x = ri(0,33)
+print(x)
+mixedList = [55, 23432, 349.50, False, True, "Hello world", [1,2,3,'a','b'], "hi", 2, x]
+for item in mixedList:
+    print(f"{item} is of data type {type(item)}")
+    
